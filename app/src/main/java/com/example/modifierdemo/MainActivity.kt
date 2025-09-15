cbbc091f542bcf94b3167bc2e83b5ad816037de9
mistifikastor <menoftoday@yandex.ru>
2025-09-15 23:08:32 +0700

    CoolImageAndText6

--- app/src/main/java/com/example/modifierdemo/MainActivity.kt
+++ app/src/main/java/com/example/modifierdemo/MainActivity.kt
@@ -12,11 +12,14 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -65,8 +68,12 @@ fun DemoScreen(modifier: Modifier = Modifier) {
         fontWeight = FontWeight.Bold
     )
     Spacer(Modifier.height(16.dp))
-    CustomImage(R.drawable.swag)
-} }
+    CustomImage(R.drawable.swag,
+        Modifier
+            .padding(16.dp)
+            .width(270.dp)
+            .clip(shape = RoundedCornerShape(30.dp)) )
+    } }
 
 
 @Preview(showBackground = true)
